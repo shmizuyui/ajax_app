fc4fc3059284ce28c01d3fdb947fc77037eca8b3
shmizuyui <yuihana760@gmail.com>
2020-08-21 17:06:13 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
     def index
-        @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
-      end
+      @posts = Post.all.order(id: "DESC")
+    end
+
+    def create
+      Post.create(content: params[:content])
+      redirect_to action: :index
+    end
 end
